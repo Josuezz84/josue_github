451f1bc2980233e28d902d3fb6713dd42b39d568
Josue Zuniga <josuezunigazuniga@gmail.com>
2025-08-13 12:35:37 -0500

    Checking the code to fix some bugs on the Bikeshare Load_data function

--- Bikeshare.py
+++ Bikeshare.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 #This is a change to be shown in git diff
+#Checking code throughout the project
 
 CITY_DATA = { 'chicago':'chicago.csv',
               'new york city': 'new_york_city.csv',
