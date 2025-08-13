63331b0ead2b9069cb4e16812f5223db556f9590
Josue Zuniga <josuezunigazuniga@gmail.com>
2025-08-13 12:36:56 -0500

    Correcting While loop logic into the Bikeshare file

--- Bikeshare.py
+++ Bikeshare.py
@@ -24,6 +24,7 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     
+    #While loop corrected
     while True: 
         try:
             while True: 
